e68b5dd44b01220844bad81913dabe8613219ee2
kamolturaev <kamolturaev@gmail.com>
2022-09-29 15:32:44 +0500

    Add files to Git

---
+++ src/ActionButton.js
@@ -0,0 +1,13 @@
+import { ACTIONS } from "./Calculator"
+
+export default function ActionButton({ dispatch, operation }) {
+  return (
+    <button
+      onClick={() =>
+        dispatch({ type: ACTIONS.CHOOSE_OPERATION, payload: { operation } })
+      }
+    >
+      {operation}
+    </button>
+  )
+}
--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import Calculator from './Calculator';
+import { width } from '@mui/system';
 
-function App() {
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+export default function BasicGrid() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box sx={{ width:'650px', marginTop: '12px', paddingLeft: '18px' }}>
+      <Grid container spacing={2}>
+        <Grid item xs={8}>
+          <Item sx={{backgroundColor:'#63A1CB'}}>
+            <Calculator/>
+          </Item>
+        </Grid>
+        {/* <Grid item xs={4}>
+          <Item>xs=4</Item>
+        </Grid>
+        <Grid item xs={4}>
+          <Item>xs=4</Item>
+        </Grid>
+        <Grid item xs={8}>
+          <Item>xs=8</Item>
+        </Grid> */}
+      </Grid>
+    </Box>
   );
 }
-
-export default App;
